40f0eed1e174aa13c375334fc2f5e90b17f9b8b3
Jeff Fansler <JeffF@FanzooTechnology.com>
2018-09-15 20:19:48 -0400

    initial commit

---
+++ config.rb
@@ -0,0 +1,130 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+#activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+ # blog.prefix = "news"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  #blog.layout = "article"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  #blog.tag_template = "tag.html"
+  #blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+#end
+
+activate :directory_indexes
+
+#page "/feed.xml", layout: false
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+require 'date'
+
+# Methods defined in the helpers block are available in templates
+ helpers do
+
+   def get_next_event(chapter)
+     today = Date.today()
+     events = chapter.events.select { |event| event.end_date >= today.strftime('%Y-%m-%d') }
+     if events.length
+       return events[0]
+     end
+     return nil
+   end
+
+   def get_previous_events(chapter)
+     today = Date.today()
+   	 prev_events = chapter.events.select { |event| event.start_date < today.strftime('%Y-%m-%d') }
+   	 prev_events.sort_by { |event| event.start_date || '1970-01-01' }
+   	 return prev_events
+   end
+
+   def get_previous_chapters()
+     prev_chapters = data.chapters.select { |key, chapter| get_previous_events(chapter).length > 0 || false }
+   end
+
+ end
+
+configure :development do
+  activate :favicon_maker, :template_dir => "source/_assets/icons/", :icons => {
+    "icon.png" => [
+      { icon: "favicon-160x160.png" },                                  # For Opera Speed Dial (up to Opera 12; this icon is deprecated starting from Opera 15), although the optimal icon is not square but rather 256x160. If Opera is a major platform for you, you should create this icon yourself.
+      { icon: "favicon-96x96.png" },                                    # For Google TV.
+      { icon: "favicon-32x32.png" },                                    # For Safari on Mac OS.
+      { icon: "favicon-16x16.png" },                                    # The classic favicon, displayed in the tabs.
+      { icon: "favicon.png", size: "16x16" },                           # The classic favicon, displayed in the tabs.
+      { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },         # Used by IE, and also by some other browsers if we are not careful.
+    ]
+  }
+end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+  activate :favicon_maker, :template_dir => "source/_assets/icons/", :icons => {
+    "icon.png" => [
+      { icon: "apple-touch-icon-180x180-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for iPhone 6 Plus with @3× display
+      { icon: "apple-touch-icon-152x152-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPad with iOS7.
+      { icon: "apple-touch-icon-144x144-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPad with iOS6 or prior.
+      { icon: "apple-touch-icon-120x120-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPhone with iOS7.
+      { icon: "apple-touch-icon-114x114-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPhone with iOS6 or prior.
+      { icon: "apple-touch-icon-76x76-precomposed.png" },               # Same as apple-touch-icon-57x57.png, for non-retina iPad with iOS7.
+      { icon: "apple-touch-icon-72x72-precomposed.png" },               # Same as apple-touch-icon-57x57.png, for non-retina iPad with iOS6 or prior.
+      { icon: "apple-touch-icon-60x60-precomposed.png" },               # Same as apple-touch-icon-57x57.png, for non-retina iPhone with iOS7.
+      { icon: "apple-touch-icon-57x57-precomposed.png" },               # iPhone and iPad users can turn web pages into icons on their home screen. Such link appears as a regular iOS native application. When this happens, the device looks for a specific picture. The 57x57 resolution is convenient for non-retina iPhone with iOS6 or prior. Learn more in Apple docs.
+      { icon: "apple-touch-icon-precomposed.png", size: "57x57" },      # Same as apple-touch-icon.png, expect that is already have rounded corners (but neither drop shadow nor gloss effect).
+      { icon: "apple-touch-icon.png", size: "57x57" },                  # Same as apple-touch-icon-57x57.png, for "default" requests, as some devices may look for this specific file. This picture may save some 404 errors in your HTTP logs. See Apple docs
+      { icon: "favicon-196x196.png" },                                  # For Android Chrome M31+.
+      { icon: "favicon-160x160.png" },                                  # For Opera Speed Dial (up to Opera 12; this icon is deprecated starting from Opera 15), although the optimal icon is not square but rather 256x160. If Opera is a major platform for you, you should create this icon yourself.
+      { icon: "favicon-96x96.png" },                                    # For Google TV.
+      { icon: "favicon-32x32.png" },                                    # For Safari on Mac OS.
+      { icon: "favicon-16x16.png" },                                    # The classic favicon, displayed in the tabs.
+      { icon: "favicon.png", size: "16x16" },                           # The classic favicon, displayed in the tabs.
+      { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },         # Used by IE, and also by some other browsers if we are not careful.
+      { icon: "mstile-70x70.png", size: "70x70" },                      # For Windows 8 / IE11.
+      { icon: "mstile-144x144.png", size: "144x144" },
+      { icon: "mstile-150x150.png", size: "150x150" },
+      { icon: "mstile-310x310.png", size: "310x310" },
+      { icon: "mstile-310x150.png", size: "310x150" }
+    ]
+  }
+end
